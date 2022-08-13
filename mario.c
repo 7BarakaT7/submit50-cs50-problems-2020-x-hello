3e9146f0bddecc9ff4f75d1719b9e36b96624144
7BarakaT7 <109825231+7BarakaT7@users.noreply.github.com>
2022-08-13 17:28:33 +0200

    Create mario.c
    #include <cs50.h>
    #include <stdio.h>
    {
        int n;
      do 
      {
        n = ger_int("width: ");
      }
       while ( n < 1);
          for (int i = 0; i < n; i++)
    {
        printf("?");
    }
    printf("\n");
    
      }
    
    
    
    }

---
+++ mario.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+{
+    int n;
+  do 
+  {
+    n = ger_int("width: ");
+  }
+   while ( n < 1);
+      for (int i = 0; i < n; i++)
+{
+    printf("?");
+}
+printf("\n");
+
+  }
+
+
+
+}
+
